4997703877144fdfc229e88c53c2ad5f541c3dd4
zunuu <zunayedhossain99@gmail.com>
2022-05-06 04:10:32 +0600

    nav-done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,57 @@
+import { Button } from 'bootstrap';
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <div className='container p-0'>
-                <Nav className="justify-content-center text-secondary  mt-3" activeKey="/home">
-                    <Nav.Item>
-                        <Link className='navItems ' to="/home">Home</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Link className='navItems ' to="/inventory">Inventory</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Link className='navItems ' to="/blogs">Blogs</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Link className='navItems ' to="/signIn">Sign In</Link>
-                    </Nav.Item>
+
+            <Navbar collapseOnSelect expand="lg" bg="dark bg-opacity-25" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <Link className='brandname' to='/'><strong className='brandname'>Spices-</strong><span className='brandname'>Den</span></Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle className='bg-success bg-opacity-75 border-0 px-3 py-2' aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="navlinks">
+                            <Nav.Link href="#home">
+                                <Link className="mx-2 " to='/home'>Home</Link>
+                            </Nav.Link>
+
+
+                            <Nav.Link href="#services">
+                                <Link className="  mx-2 " to='/inventory'>Inventory</Link>
+                            </Nav.Link>
 
 
 
-                </Nav>
-            </div >
-        </div>
+
+
+                            <Nav.Link href="#blogs">
+                                <Link className=" mx-2 " to='/blogs'>Blogs</Link>
+                            </Nav.Link>
+
+
+                            <Nav.Link href="#aboutme">
+                                <Link className=" mx-2 " to='/signIn'>Sign In</Link>
+                            </Nav.Link>
+
+
+
+
+
+
+
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+
+            </Navbar>
+
+        </div >
+
     );
 };
 
--- src/Components/Main/Homepage/Home/Banner.js
+++ src/Components/Main/Homepage/Home/Banner.js
@@ -1,8 +1,9 @@
+import { Button } from 'bootstrap';
 import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
+        <div className=''>
             <h1>This is banner (hulk)</h1>
         </div>
     );
